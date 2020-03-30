b1077b5cb5598661633ab5b933150bbfeb1d3a17
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-30 15:03:14 +0300

    rendering tweets on timeline

---
+++ resources/js/components/timeline/AppTimeline.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{ tweets }}
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        data () {
+            return {
+                tweets: []
+            }
+        },
+
+        methods: {
+            async getTweets() {
+                let response = await axios.get('/api/timeline')
+
+                this.tweets = response.data.data
+            }
+        },
+
+        mounted() {
+            this.getTweets()
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
             nav
         </div>
 
-        <div class="w-7/12 border border-gray-800 border-t-0 ">
-            timeline
+        <div class="w-7/12 border border-gray-800 border-t-0 border-b-0">
+            <app-timeline />
         </div>
     </div>
 @endsection
