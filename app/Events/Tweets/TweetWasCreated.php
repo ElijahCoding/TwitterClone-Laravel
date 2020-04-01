465189be41c53e21817c8b71220d5ebae2cb0bc1
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-01 19:30:36 +0300

    broadcasting when created tweet

---
+++ app/Events/Tweets/TweetWasCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events\Tweets;
+
+use App\Tweet;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TweetWasCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $tweet;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Tweet $tweet)
+    {
+        $this->tweet = $tweet;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
