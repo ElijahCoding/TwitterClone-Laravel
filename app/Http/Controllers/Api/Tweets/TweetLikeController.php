be47e7efe71a3b3ddee2e864e8b4be14894d0eed
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-06 00:09:03 +0300

    like a tweet api

---
+++ app/Http/Controllers/Api/Tweets/TweetLikeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Tweets;
+
+use App\Http\Controllers\Controller;
+use App\Tweet;
+use Illuminate\Http\Request;
+
+class TweetLikeController extends Controller
+{
+    public function store(Tweet $tweet, Request $request)
+    {
+
+    }
+}
