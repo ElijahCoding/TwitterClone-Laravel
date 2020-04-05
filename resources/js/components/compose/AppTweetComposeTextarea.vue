bc0d7b12200160c58817ac01a833026b00659ff6
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-05 15:57:00 +0300

    refactored textarea into a seperate component

---
+++ resources/js/components/compose/AppTweetComposeTextarea.vue
@@ -0,0 +1,19 @@
+<template>
+    <textarea
+        class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
+        placeholder="What's happening ?"
+        autofocus
+        @input="$emit('input', $event.target.value)"
+        :value="value"
+    />
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                required: false
+            }
+        }
+    }
+</script>
