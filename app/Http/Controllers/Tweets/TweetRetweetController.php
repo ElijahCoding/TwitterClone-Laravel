159f0635873d9fbbf461f2e6833875d0a208a1c4
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-24 16:47:34 +0300

    building retweet function with broadcasting

---
+++ app/Http/Controllers/Tweets/TweetRetweetController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Tweets;
+
+use App\Http\Controllers\Controller;
+use App\Tweet;
+use Illuminate\Http\Request;
+
+class TweetRetweetController extends Controller
+{
+    public function store(Tweet $tweet, Request $request)
+    {
+
+    }
+
+    public function destroy(Tweet $tweet, Request $request)
+    {
+
+    }
+}
