39a7666de94b0d29c05fabe33085b8ff1f287a62
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-14 13:58:45 +0300

    building entities , hashtag mention

--- app/Tweet.php
+++ app/Tweet.php
@@ -48,4 +48,9 @@ class Tweet extends Model
     {
         return $this->hasMany(Tweet::class, 'parent_id', 'id');
     }
+
+    public function entities()
+    {
+        return $this->hasMany(Entity::class);
+    }
 }
