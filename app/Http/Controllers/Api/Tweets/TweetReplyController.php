9fcc400c4c9d4614b249775f7321e93d5183c04c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-09 20:13:17 +0300

    tweet replies

--- app/Http/Controllers/Api/Tweets/TweetReplyController.php
+++ app/Http/Controllers/Api/Tweets/TweetReplyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Tweets;
 
 use App\Events\Tweets\TweetRepliesWereUpdated;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TweetCollection;
 use App\Notifications\Tweets\TweetRepliedTo;
 use App\Tweet;
 use App\TweetMedia;
@@ -15,6 +16,11 @@ class TweetReplyController extends Controller
     public function __construct()
     {
         $this->middleware(['auth:sanctum'])->only('store');
+    }
+
+    public function show(Tweet $tweet)
+    {
+        return new TweetCollection($tweet->replies);
     }
 
     public function store(Tweet $tweet, Request $request)
